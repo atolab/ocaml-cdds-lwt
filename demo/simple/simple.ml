541e6e2961e66ec60eff6dad2e5d96a30079bb5b
Angelo Corsaro <angelo@icorsaro.net>
2018-04-16 01:03:52 +0200

    updated example to use recursion

--- demo/simple/simple.ml
+++ demo/simple/simple.ml
@@ -1,19 +1,15 @@
 open Cdds_lwt
 open Lwt.Infix
 
-let dp = Participant.make DomainId.default
-let name = "KeyValue"
-let topic = Topic.make dp name
-let pub = Publisher.make dp
-let sub = Subscriber.make dp
-let dr = Reader.make sub topic
-let read_loop () =
-  let rec make_n_comp n cs =
-    let data = Cdds_lwt.Reader.sread dr Duration.infinity in
-    let comp = data >>= (fun s -> List.iter (fun ((k,v),_) -> let _ = Lwt_io.printf "[%d]>> key: %s, value: %s\n" n k (Bytes.to_string v) in ()) s; Lwt.return_unit ) in
-    if n = 0 then cs else make_n_comp (n-1 )(comp :: cs)
-  in
-  let comps = make_n_comp 10000 [] in
-  Lwt.join comps
+let _ =
+  let dp = Participant.make DomainId.default in
+  let name = "KeyValue" in
+  let topic = Topic.make dp name in
+  let sub = Subscriber.make dp in
+  let dr = Reader.make sub topic in
 
-let _ = Lwt_main.run @@ read_loop ()
+  let rec read_data r =
+    let%lwt data = Cdds_lwt.Reader.sread r Duration.infinity in
+    List.iter (fun ((k,v),_) -> let _ = Lwt_io.printf ">>> key: %s, value: %s\n" k (Bytes.to_string v) in ()) data ;
+    read_data dr
+  in Lwt_main.run @@ read_data dr
