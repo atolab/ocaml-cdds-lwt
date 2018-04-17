2f1d56d48829b1bc035352d8475720a59d1e2c89
gabrik <gabriele.baldoni@gmail.com>
2018-04-17 15:36:34 +0200

    Added writer example

--- demo/simple/simple.ml
+++ demo/simple/simple.ml
@@ -1,15 +1,44 @@
 open Cdds_lwt
 open Lwt.Infix
 
-let _ =
-  let dp = Participant.make DomainId.default in
-  let name = "KeyValue" in
-  let topic = Topic.make dp name in
+
+let dp = Participant.make DomainId.default
+let name = "KeyValue"
+let topic = Topic.make dp name
+
+let reader () =
   let sub = Subscriber.make dp in
   let dr = Reader.make sub topic in
-
   let rec read_data r =
     let%lwt data = Cdds_lwt.Reader.sread r Duration.infinity in
     List.iter (fun ((k,v),_) -> let _ = Lwt_io.printf ">>> key: %s, value: %s\n" k (Bytes.to_string v) in ()) data ;
     read_data dr
-  in Lwt_main.run @@ read_data dr
+  in
+  Lwt_main.run @@ read_data dr
+
+let writer () =
+  let w = Writer.make dp topic in
+  let rec write_data w n =
+    let k = "ocaml" ^ (string_of_int (n mod 10)) in
+    let v = "rulez-" ^ (string_of_int n) in
+    let%lwt r = Writer.write_string w k v in
+    let _ = Lwt_io.printf "Write %d returned %d\n" n @@ Int32.to_int r in ();
+    Unix.sleepf 0.01;
+    write_data w @@ n - 1
+  in
+    Lwt_main.run @@ write_data w 10000
+
+
+
+
+let usage () = ignore( print_endline "USAGE:\n\t simple <pub | sub>" )
+
+
+let _ =
+  let argv = Sys.argv in
+  if Array.length argv < 2 then usage ()
+  else
+  match Array.get argv 1 with
+    | "pub" -> writer ()
+    | "sub" -> reader ()
+    | _ -> usage ()
