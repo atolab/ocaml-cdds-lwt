ccebbbc80fcf0ce6e5f9ba4b4eded9e364c67a5d
gabrik <gabriele.baldoni@gmail.com>
2018-04-23 14:11:18 +0200

    example now crash for assertion failed on key hash

--- demo/simple/simple.ml
+++ demo/simple/simple.ml
@@ -11,20 +11,24 @@ let reader () =
   let dr = Reader.make sub topic in
   let rec read_data r =
     let%lwt data = Cdds_lwt.Reader.sread r in
-    List.iter (fun ((k,v), _) -> let _ = Lwt_io.printf ">>> key: %s, value: %s\n" k (Bytes.to_string v) in ()) data ;
+    List.iter (fun ((k,v), _) -> ()
+        (* ignore @@ Lwt_io.printf ">>> key: %s, value: %s\n" k (Bytes.to_string v); *)
+      )
+        data;
     read_data dr
   in
   Lwt_main.run @@ read_data dr
 
 let writer () =
+  let b = Bytes.make 1500 'A' in
   let pub = Publisher.make dp in
   let w = Writer.make pub topic in
   let rec write_data w n =
     let k = "ocaml" ^ (string_of_int (n mod 10)) in
-    let v = "rulez-" ^ (string_of_int n) in
+    let v = (Bytes.unsafe_to_string b) ^ "-rulez-" ^ (string_of_int n) in
     let%lwt r = Writer.write_string w k v in
-    let _ = Lwt_io.printf "Write %d returned %d\n" n @@ Int32.to_int r in ();
-    Unix.sleepf 0.1;
+    (* let _ = Lwt_io.printf "Write %d returned %d\n" n @@ Int32.to_int r in (); *)
+    (* Unix.sleepf 0.1; *)
     write_data w @@ n - 1
   in
     Lwt_main.run @@ write_data w 10000
